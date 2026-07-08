78f300a0dc25e95bd10cdec645606c01e614ae85
Zohreh Salehi <salehii.zhaleh@gmail.com>
2026-07-08 23:21:29 +0200

    Verify dotenv implementation

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,13 +1,26 @@
 import requests
+import os
+from dotenv import load_dotenv
 
-API_KEY = "ibAb9WY4Vxv2y9PK5H61ODXMc8LCzFmpZFigsrCc"
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
 API_URL = "https://api.api-ninjas.com/v1/animals"
 
+
 def fetch_data(animal_name):
+    """
+    Fetches the animals data for the animal 'animal_name'.
+
+    Returns: a list of animals
+    """
     params = {"name": animal_name}
     headers = {"X-Api-Key": API_KEY}
+
     response = requests.get(API_URL, params=params, headers=headers)
+
     if response.status_code == 200:
         return response.json()
     else:
+        print(f"Error: {response.status_code}")
         return []
